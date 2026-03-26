f24f1255cbca8a25aaa6dbdf15b7f252b9da686e
Eamonn Upperton <uppertoe@gmail.com>
2026-03-26 23:49:45 +1100

    feat: enable default caddy compression

--- docker/run-caddy.sh
+++ docker/run-caddy.sh
@@ -7,7 +7,24 @@ cp /etc/caddy/Caddyfile "$generated_config"
 
 find /srv/repo/apps -mindepth 2 -maxdepth 2 -type f -name '*.caddy' | sort | while read -r snippet; do
   printf '\n' >> "$generated_config"
-  cat "$snippet" >> "$generated_config"
+  awk '
+    {
+      line = $0
+      trimmed = line
+      opens = gsub(/\{/, "{", line)
+      closes = gsub(/\}/, "}", line)
+      sub(/^[[:space:]]*/, "", trimmed)
+
+      print $0
+
+      # Inject default response compression into each top-level site block.
+      if (depth == 0 && line ~ /\{[[:space:]]*$/ && substr(trimmed, 1, 1) != "(") {
+        print "    encode zstd gzip"
+      }
+
+      depth += opens - closes
+    }
+  ' "$snippet" >> "$generated_config"
   printf '\n' >> "$generated_config"
 done
 
